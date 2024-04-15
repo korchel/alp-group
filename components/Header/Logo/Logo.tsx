38c76faf246c552d30d9111bccab31b7488b2689
korchel <korchel21@gmail.com>
2024-04-15 13:00:27 +0300

    logo link added

--- components/Header/Logo/Logo.tsx
+++ components/Header/Logo/Logo.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import logo from "./logo.png";
 import styles from "./Logo.module.scss";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <div className={styles.logo}>
+    <Link className={styles.logo} href={'/'}>
       <Image src={logo} alt="Kapibara Lab" height={29} />
       <p>Современные методики диагностики</p>
-    </div>
+    </Link>
   );
 };
